66a2027e578613d1ba2ee223c0e5489a8d8d2a38
LEAKONO <leakonoemmanuel3@gmail.com>
2025-07-22 22:48:13 +0300

    feat:added homepage and about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+        <h1>About Me</h1>
+        <p>I’m a Kenya-based fullstack developer with strong experience in JavaScript, React, Node.js, and Express. I’ve built and deployed multiple fullstack projects from the ground up, including a Doctor’s Appointment System and an Asset Inventory Management System, integrating features like authentication, role-based access, image uploads (Cloudinary), and secure API endpoints.</p>
+        <p>I thrive in early-stage product development, having worked on pre-MVP products where I collaborated across backend and frontend to ship usable features fast. I’ve also handled deployment using platforms like Render, Vercel, and MongoDB Atlas, ensuring smooth end-to-end delivery.</p>
+        <p>Outside of coding, I’m a curious learner who’s always exploring new tools, especially in the JavaScript and DevOps ecosystem. My long-term goal is to build and scale impactful platforms that serve real users — and to grow as a thoughtful, collaborative developer who brings clarity and value to any engineering team.
+
+</p>
+        
+</div>
   )
 }
 
